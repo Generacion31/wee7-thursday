2639453f8823fd5366eaa83a1d020d71daa136d9
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-10-12 21:52:56 -0300

    advances

---
+++ main.js
@@ -0,0 +1,42 @@
+// Ejemplo de destructuración de objetos
+const persona = { nombre: 'John', edad: 30, ciudad: 'Nueva York', estado: 'casado' };
+
+
+// Ejemplo de destructuración de arreglos
+const colores = ['rojo', 'verde', 'azul', 'green'];
+
+
+// Ejemplo del operador spread
+const arreglo1 = [1, 2, 3];
+const arreglo2 = [4, 5, 6];
+
+
+// Ejemplo del operador rest
+
+
+
+// Ejemplo de forEach
+const numeros2 = [1, 2, 3, 4, 5];
+
+
+// Ejemplo de reduce
+const numeros = [1, 2, 3, 4, 5];
+
+
+// Ejemplo de map
+
+
+
+// Ejemplo de filter
+const personas = [
+  { nombre: "Alice", edad: 25 },
+  { nombre: "Bob", edad: 30 },
+  { nombre: "Carol", edad: 28 },
+  { nombre: "David", edad: 35 },
+  { nombre: "Eve", edad: 22 }
+];
+
+// Ejemplo de find -> devuelve el primero que encuentra
+
+
+// Ejemplo de funciones flecha
